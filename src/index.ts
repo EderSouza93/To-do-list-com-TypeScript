4b7d19825baafaf5bcde8096bb6ae8e102dfccfb
EderSouza93 <84224107+EderSouza93@users.noreply.github.com>
2023-04-23 21:56:24 -0300

    To do e Reminders

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,41 @@
-alert('Bora iniciar esse projeto de TS. :)');
+(() => {
+    const todo = {
+        description: 'todo',
+        done: false,
+    };
+
+    const remind = {
+        description: 'reminder',
+        date: '23.04.2023',
+    };
+
+    const taskView = {
+        render(tasks: Array<Object>) {
+            const tasksList = document.getElementById('tasksList');
+            while (tasksList?.firstChild) {
+                tasksList.removeChild(tasksList.firstChild);
+            }
+
+            tasks.forEach((task) => {
+                const li = document.createElement('LI');
+                const textNode = document.createTextNode(JSON.stringify(task));
+                li.appendChild(textNode);
+                tasksList?.appendChild(li);
+            });
+        },      
+    };
+
+    const TaskController = (view: typeof taskView) => {
+        const tasks: Array<Object> = [todo, remind];
+
+        const handleEvent = (event:Event) => {
+            event.preventDefault();
+            view.render(tasks);
+        }
+        
+        document.getElementById('taskForm')
+        ?.addEventListener('submit', handleEvent);
+    };
+
+    TaskController(taskView);
+})();
